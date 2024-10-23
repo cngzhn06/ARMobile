40a1da5dbc44d36c6dcec7a6d988e03363c33add
cengizhan <cngzhnc06@gmail.com>
2024-10-24 00:46:40 +0300

    deneme push

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-
 import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
+  Text,
+  View,
 } from 'react-native';
 
 
@@ -11,13 +12,20 @@ function App(): React.JSX.Element {
 
   return (
     <SafeAreaView style={styles.container}>
-    
+      <View>
+        <Text>
+          ARTIRILMIŞ GERÇEKLİK UYGULAMASI
+        </Text>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
     
   },
 });
